36ff090e0ca07bc132867e368442ea64277f82c4
Christopher de Oliveira <christopherforwork@gmail.com>
2021-03-11 12:55:56 -0400

    🚧 (Controllers): Implemented a controller for each model

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const {
-  UserController
+  AdController,
+  AuthController,
+  CategoryController,
+  StateController,
+  UserController,
 } = require('../app/controllers');
 
 router.get('/ping', (req, res) => {
@@ -9,5 +13,24 @@ router.get('/ping', (req, res) => {
     pong: true,
   });
 });
+
+router.get('/states', StateController.getStates);
+
+// router.post('/user/signin', AuthController.signIn);
+
+// router.post('/user/signup', AuthController.signUp);
+
+// router.get('/user/me', UserController.info);
+// router.put('/user/me', UserController.editAction);
+
+// router.get('/categories', CategoryController.getCategories);
+
+// router.get('/ad/item', AdController.getItem);
+
+// router.get('/ad/list', AdController.getList);
+
+// router.post('/ad/add', AdController.addAction);
+
+// router.post('/ad/:public_id', AdController.editAction);
 
 module.exports = router;
