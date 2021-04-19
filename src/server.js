29c8f83d027d9fa4a7fdc2ae445bcaae4c18ec2a
Christopher de Oliveira <christopherforwork@gmail.com>
2021-04-19 10:54:25 -0400

    🚩 (Heroku): Added an env variable to execute database cleaning

--- src/server.js
+++ src/server.js
@@ -28,4 +28,6 @@ server.listen(process.env.PORT, () => {
   console.log(`Server running on port: ${process.env.PORT}`);
 });
 
-ResetController.resetAdsOnStart();
+if(process.env.IS_HEROKU){
+  ResetController.resetAdsOnStart();
+};
