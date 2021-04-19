584dc4fae99b6fc4858057f24178789c4dc612a4
Christopher de Oliveira <christopherforwork@gmail.com>
2021-04-19 11:54:22 -0400

    🐛 (BUG): Fixed conditioning bug

--- src/server.js
+++ src/server.js
@@ -28,6 +28,6 @@ server.listen(process.env.PORT, () => {
   console.log(`Server running on port: ${process.env.PORT}`);
 });
 
-if(process.env.IS_HEROKU){
+if(process.env.IS_HEROKU === 'true'){
   ResetController.resetAdsOnStart();
 };
